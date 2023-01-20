126dcb7f1f887885b7e5299e8a568f695a4a8d88
PigPenguinMan <bigpenguinman@naver.com>
2023-01-20 14:23:49 +0900

    0120 test

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script >
+ export default{ 
+  data() {
+    return{
+      count : 5
+    }
+  },
+  methods: {
+    increment(){
+      this.count ++ 
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+  <h3>vue로 확인하는 웹페이지 입니다 . 아래버튼을 누르면 값이 1씩 증가합니다</h3>
+  <button v-on:click="increment"> 1+ </button>
+</template>
+
